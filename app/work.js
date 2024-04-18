306ece479acd9abb7febec06538dd4516ed9412b
rarepython21 <frussodesigns@gmail.com>
2024-04-18 14:51:22 -0400

    work r1

---
+++ app/work.js
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function work() {
+  return (
+    <View>
+      <Text>splash mtv</Text>
+      <Text>lokt</Text>
+      <Text>llc filer</Text>
+      <Text>nfc shirt</Text>
+      <Text>paige</Text>
+      <Text>choldrens</Text>
+      <Text>bookworm</Text>
+    </View>
+  )
+}
